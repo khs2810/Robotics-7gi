9fb9619797fe5c3fc381ede8aecfb233a0dbc74b
hyeonseo <khs_2810@naver.com>
2025-12-19 10:52:20 +0900

    feat. Conveyor 작성
    - Conveyor.cs 작성 시작

---
+++ Assets/251218/Conveyor.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+/// <summary>
+/// PLC의 Y00(정방향회전), Y01(역방향회전) 신호를 받아, Dragger를 특정 위치로 특정 속도로 이동시킨다.
+/// 속성: Dragger
+/// </summary>
+public class Conveyor : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
